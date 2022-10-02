a496309c6e2f81401f5de2023b99cd27667891d1
Marshall Chan <marshallchan666@outlook.com>
2022-10-02 14:47:03 +0800

    feat: articles list initialized, interface design

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
+import styles from './index.module.scss';
 
 const Footer: NextPage = () => {
-  return <div>页脚</div>;
+  return (
+    <div className={styles.footer}>
+      <p>nice try</p>
+    </div>
+  );
 };
 
 export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,46 @@
 import type { NextPage } from 'next';
+import { Divider } from 'antd';
 
-const Home: NextPage = () => {
-  return <div>首页</div>;
+import AppDataSource from 'db';
+import { Article } from 'db/entities';
+import ListItem from 'components/LIstItem';
+import { IArticle } from './api';
+
+interface IProps {
+  articles: IArticle[];
+}
+
+const Home = (props: IProps) => {
+  const { articles } = props;
+  console.log(articles);
+  return (
+    <div className="contentLayout">
+      {articles?.map((article) => {
+        return (
+          <>
+            <ListItem article={article} key={article?.id}></ListItem>
+            <Divider />
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
+
+export async function getServerSideProps(context) {
+  if (!AppDataSource.isInitialized) await AppDataSource.initialize();
+  const articleRepo = await AppDataSource.getRepository(Article);
+  const articles = await articleRepo.find({
+    relations: {
+      user: true,
+    },
+  });
+
+  return {
+    props: {
+      articles: JSON.parse(JSON.stringify(articles)) || [],
+    }, // will be passed to the page component as props
+  };
+}
